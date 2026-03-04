9ac7060efb562e816978b9a808d5cee8179566f1
jai gaigoye <jai.22210326@viit.ac.in>
2026-03-04 19:11:13 +0530

    update API endpoint for deployment

--- frontend/src/CognitiveEditor.js
+++ frontend/src/CognitiveEditor.js
@@ -47,7 +47,7 @@ const analyzeLoad = async ()=>{
 try{
 
 const res = await axios.post(
-"http://localhost:5000/api/load",
+"https://cognitive-load-ui.onrender.com/api/load",
 {
 typingSpeed:keystrokes,
 backspaces,
